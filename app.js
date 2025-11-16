0aa35eb62955867e625855de42755f6ddedb0ecb
HUYNH PHUOC HUNG B2205941 <hungb2205941@student.ctu.edu.vn>
2025-11-16 21:46:30 +0700

    CAI DAT XU LY LOI

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const ApiError = require('./app/api-error');
 const app = express();
 const bookRoutes = require('./app/routes/book.route');
 app.use('/api/books', bookRoutes);
+//handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, 'Resource not found'));
+});
+//handle other errors
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || 'Internal Server Error',
+  });
+});
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to book application.' });
 });
+
 module.exports = app;
