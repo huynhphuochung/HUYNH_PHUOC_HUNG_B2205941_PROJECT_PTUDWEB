eb020b9bae7f98ad095ec88f32114240f505edc6
HUYNH PHUOC HUNG B2205941 <hungb2205941@student.ctu.edu.vn>
2025-11-17 00:24:42 +0700

    Cai dat frontend

---
+++ book-frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+ 
+};
+</script>
+<template>
+  <div id="app">
+    <h1>Welcome to the Book Application</h1>
+  </div>
+</template>
+<style>
+.page{
+  max-width:400px;
+  margin: auto;
+}
+</style>
